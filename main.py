e027d7a5d34ca34e39b2c189ec167003da7139d2
Redover <67553709+RedoverPlayer@users.noreply.github.com>
2021-03-25 17:32:59 +0100

    Speed not based on fps now, prep for other update

--- main.py
+++ main.py
@@ -56,8 +56,13 @@ for y in map:
 
 running = True
 
+clock = pygame.time.Clock()
+
 # main loop
 while running:
+    # The game runs at max 144 FPS (a little less due to the time of computation of each frame)
+    tick_time = clock.tick(144)
+
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -68,38 +73,50 @@ while running:
 
     # changing player coords when pressing keys
     pressed_keys = pygame.key.get_pressed()
+    movement_speed = 2 * tick_time / 8
     if pressed_keys[pygame.K_q]:
-        if player_coords[0] > 40:
-            player_coords[0] -= 5
+        if player_coords[0] - movement_speed > 38:
+            player_coords[0] -= movement_speed
+        else:
+            player_coords[0] -= player_coords[0] - 38
     if pressed_keys[pygame.K_d]:
-        if player_coords[0] < map_size * tile_size - 40:
-            player_coords[0] += 5
+        if player_coords[0] + movement_speed < map_size * tile_size - 38:
+            player_coords[0] += movement_speed
+        else:
+            player_coords[0] -= player_coords[0] - (map_size * tile_size - 38)
     if pressed_keys[pygame.K_z]:
-        if player_coords[1] > 40:
-            player_coords[1] -= 5
+        if player_coords[1] - movement_speed > 38:
+            player_coords[1] -= movement_speed
+        else:
+            player_coords[1] -= player_coords[1] - 38
     if pressed_keys[pygame.K_s]:
-        if player_coords[1] < map_size * tile_size - 40:
-            player_coords[1] += 5
+        if player_coords[1] + movement_speed < map_size * tile_size - 38:
+            player_coords[1] += movement_speed
+        else:
+            player_coords[1] -= player_coords[1] - (map_size * tile_size - 38)
 
     # background color
     screen.fill((50, 50, 50))
 
     # adding map tiles to the screen
-    tiles_rendered = sum([map_tile.update(screen, player_coords) for map_tile in map_tiles])
-    # for map_tile in map_tiles:
-    #     map_tile.update(screen, player_coords)
-    
+    tiles_rendered = [elem for elem in [map_tile.update(screen, player_coords) for map_tile in map_tiles] if elem != None]
+
+    # for map_tile in tiles_rendered:
+    #     if player.rect.colliderect(map_tile.rect):
+    #         if map_tile.type == "wall":
+    #             print("hey")
+
     screen.blit(player.surf, (SCREEN_WIDTH/2 - 75/2, SCREEN_HEIGHT/2 - 75/2))
 
     # debug
     playercoords = myfont.render(str(player_coords), True, (250, 250, 250))
     screen.blit(playercoords, (5, 5))
     
-    tiles_number = myfont.render("Rendered tiles : " + str(tiles_rendered), True, (250, 250, 250))
+    tiles_number = myfont.render("Rendered tiles : " + str(len(tiles_rendered)), True, (250, 250, 250))
     screen.blit(tiles_number, (5, 50))
 
     # render elements to the screen
     pygame.display.flip()
 
-    # calibrated for ~ 125 Hz
-    time.sleep(0.008)
+    # calibrated for 100 Hz
+    # time.sleep(0.01)
--- map_tile.py
+++ map_tile.py
@@ -3,6 +3,7 @@ from pygame.cursors import tri_left
 
 class MapTile(pygame.sprite.Sprite):
     def __init__(self, screen_width, screen_height, x, y, tile_size, type="wall"):
+        print(x, y)
         super(MapTile, self).__init__()
         self.surf = pygame.Surface((tile_size, tile_size))
         self.surf.fill((255, 115, 0) if type == "wall" else (0, 140, 255) if type == "water" else (0, 81, 0))
@@ -13,10 +14,17 @@ class MapTile(pygame.sprite.Sprite):
         self.type = type
         self.tile_size = tile_size
 
+        # Rect coords
+        self.top_left = (x, y)
+        self.top_right = (x + tile_size, y)
+
+        self.bottom_right = (x + tile_size, y + tile_size)
+        self.bottom_left = (x, y + tile_size)
+
     def update(self, screen, player_coords):
         # add the tile to the screen the tile if its coordinates are on the window
         if ((self.coords[0] - player_coords[0]) + self.screen_width / 2) > -1 * self.tile_size and ((self.coords[0] - player_coords[0]) + self.screen_width / 2) < self.screen_width and (-1 * player_coords[1] - self.coords[1]) + self.screen_height / 2 > -1 * self.tile_size and (-1 * player_coords[1] - self.coords[1]) + self.screen_height / 2 < self.screen_height + self.tile_size:
             screen.blit(self.surf, ((self.coords[0] - player_coords[0]) + self.screen_width / 2, (-1 * player_coords[1] - self.coords[1]) + self.screen_height / 2))
-            return 1
+            return self
         else:
-            return 0
+            return None
