af38736edd4e08c18622c0e88c1696ad60e5615a
Redover <67553709+RedoverPlayer@users.noreply.github.com>
2021-04-20 15:22:43 +0200

    Removed unused code

--- server/coords_handler.py
+++ server/coords_handler.py
@@ -9,9 +9,3 @@ def coords_handler(udp_socket, data, connected_clients, addr, users, games):
                     tmp = client
             for c in [elem for elem in connected_clients if elem["auth_token"] in [users[elem]["auth_token"] for elem in users if elem in game.clients and elem != tmp]]:
                 udp_socket.sendto(('{"type": "remote_coords", "id": "' + data["id"] + '", "coords": ' + str(data["coords"]) + '}').encode(), c["addr"])
-                
-        # for client in [elem["addr"] for elem in connected_clients if elem["addr"] != addr]:
-        #     try:
-        #         udp_socket.sendto(('{"type": "remote_coords", "id": "' + data["id"] + '", "coords": ' + str(data["coords"]) + '}$').encode(), client)
-        #     except:
-        #         connected_clients.remove([address for address in connected_clients if address == client][0])
