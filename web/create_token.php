4a4a402cab8d79dce035ae6f68bd1f19eaa5af68
CercleTrone <62258731+CercleTrone@users.noreply.github.com>
2021-04-30 15:52:44 +0200

    proper message

--- web/create_token.php
+++ web/create_token.php
@@ -17,13 +17,15 @@
     //0 for email verif and 1 for rest password
     if ($_SESSION['email_type']==0){
         SendMail($_SESSION['email'], "Verify your email", "<h1>Jeu en cours de développement, nom sujet à modifications </h1><h2>Please verify your email by clicking this link : <a href='http://" . $_SERVER['HTTP_HOST'] . "/projectNSI/verif_email.php?token_id=" . $token_id . "'>Verify your email</a></h2>");
+        print("<p class='info'>you need to verify your email, one has just been sent to you so check your mail box</p>");
     } elseif ($_SESSION['email_type']==1){
         SendMail($_SESSION['email'], "Verify your email", "<h1>Jeu en cours de développement, nom sujet à modifications </h1><h2>you can change your password by clicking this link : <a href='http://" . $_SERVER['HTTP_HOST'] . "/projectNSI/reset_pass.php?token_id=" . $token_id . "'>Reset your password</a></h2>");
+        print("<p class='info'>A email has been sent, you need to check it if you want to reset your password");
     }
 
-    if (isset($_SERVER['HTTP_REFERER'])) {
-        header("Location: ".$_SERVER['HTTP_REFERER']);
-    } else {
-        header("Location: index.php");
-    }
+    // if (isset($_SERVER['HTTP_REFERER'])) {
+    //     header("Location: ".$_SERVER['HTTP_REFERER']);
+    // } else {
+    //     header("Location: index.php");
+    // }
 ?>    
